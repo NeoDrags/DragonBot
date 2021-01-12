4b374b1bb0486f07eb9f4ea287bf2920d44b7075
Neodrags <thegifat@gmail.com>
2021-01-12 15:58:50 +0530

    ready

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext.commands import Bot
 from discord.utils import get
 import os
+import json
 
 load_dotenv()
 
@@ -11,23 +12,40 @@ client = Bot(command_prefix ="$")
 @client.event
 async def on_ready():
     game = discord.Game("Ready to roar!")
-    await client.change_presence(status=discord.Status.online, activity=game)
+    await client.change_presence(status=discord.Status.dnd, activity=game)
     print("Connected to Discord as: " + client.user.name)
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f":table_tennis: smashed at you wih a ping of {client.latency}")
+    '''
+    Gets the latency of the bot
+    '''
+    await ctx.send(f":table_tennis: smashed at you wih a ping of {client.latency} ms")
+
+f = open('badword.json')
+arr = json.load(f)["BadWords"]
+
+# @client.event
+# async def onSwear(message):
+    
 
 @client.command()
 async def verify(message):
+    '''
+    Verifies you NOTE: This only works if you have a role name called Members with no aliteration in spelling or name
+    '''
     member = message.author
     role = discord.utils.get(message.guild.roles, name = "Members")
     await member.add_roles(role)
     await message.channel.send(f"{member.name} has been verified")
 
 @client.command()
-async def say(message, * , text):
-    await message.channel.send(f"{text}")
+async def say(ctx, * , text):
+    '''
+    Echos what you say
+    '''
+    await ctx.message.delete()
+    await ctx.send(f"{text}")
 
 print("Running the bot...")
 try:
